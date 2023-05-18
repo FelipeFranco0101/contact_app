a75b802e790a1dc8aa69b0e19bab3c2ab60aaed8
diazgalvis <diazgalvis@live.com>
2023-05-18 10:49:53 -0500

    Merge branch 'main' of https://github.com/FelipeFranco0101/contact_app

--- lib/contact_home_page.dart
+++ lib/contact_home_page.dart
@@ -23,78 +23,89 @@ class ContactHomeState extends State<ContactHomePages> {
     super.initState();
     getAmounPeople();
     getAverage();
-    setState(() { });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Contactos    media: $promedioEdad'),
-        backgroundColor: Colors.teal,
-        actions: [
-          Center(
-            child: badges.Badge(
-              position: badges.BadgePosition.topEnd(top: -12, end: 18),
-              badgeContent: Text('$amountPeopel', style: const TextStyle(color: Colors.white),),
-              badgeStyle: const badges.BadgeStyle(
-                badgeColor: Colors.orange,
-                padding: EdgeInsets.all(5)
-              ),
-              child: const Icon(Icons.people),
-            ),
-          )
-        ],
-      ),
-      body: FutureBuilder(
-        future: databaseHelper.retrieveContacs(), 
-        builder: (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          //getAmounPeople();
-          List<Contact> lstContact = snapshot.data!;
-          //getAmounPeopleTest(lstContact.length);
-          return ListView.builder(
-            itemCount: lstContact.length,
-            itemBuilder: (BuildContext context, int index) {
-              //amountPeopel = lstContact.length;
-              //getAmounPeople();
-              Contact contact = lstContact[index];
-              return Card(
-                color: Colors.white,
-                elevation: .5,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
-                    child: Text(contact.nombres[0]+contact.apellidos[0]),
-                  ),
-                  title: Text("${contact.nombres} ${contact.apellidos}"),
-                  subtitle: Text("Edad: ${contact.edad} \nCel: ${contact.telefono} \nEmail: ${contact.email} "),
-                  onTap: () {
-                    debugPrint("Accion para editar el contacto");
-                    navigateToDetail(contact, 'Editar Contacto');
-                  },
+        appBar: AppBar(
+          title: Text('Promedio de Edad: $promedioEdad'),
+          backgroundColor: Colors.teal,
+          actions: [
+            Center(
+              child: badges.Badge(
+                position: badges.BadgePosition.topEnd(top: -12, end: 18),
+                badgeContent: Text(
+                  '$amountPeopel',
+                  style: const TextStyle(color: Colors.white),
                 ),
-              );
-            });
-        }
-      ),  
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => FormContact(database: database,))).then((value) => reloadContacts())
-        },
-        label: const Text('Contact'),
-        icon: const Icon(Icons.person_add),
-      )
-    );
+                badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.orange, padding: EdgeInsets.all(5)),
+                child: const Icon(Icons.people),
+              ),
+            )
+          ],
+        ),
+        body: FutureBuilder(
+            future: databaseHelper.retrieveContacs(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              //getAmounPeople();
+              List<Contact> lstContact = snapshot.data!;
+              //getAmounPeopleTest(lstContact.length);
+              return ListView.builder(
+                  itemCount: lstContact.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    //amountPeopel = lstContact.length;
+                    //getAmounPeople();
+                    Contact contact = lstContact[index];
+                    return Card(
+                      color: Colors.white,
+                      elevation: .5,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor:
+                              Color((Random().nextDouble() * 0xFFFFFF).toInt())
+                                  .withOpacity(1.0),
+                          child:
+                              Text(contact.nombres[0] + contact.apellidos[0]),
+                        ),
+                        title: Text("${contact.nombres} ${contact.apellidos}"),
+                        subtitle: Text(
+                            "Edad: ${contact.edad} \nCel: ${contact.telefono} \nEmail: ${contact.email} "),
+                        // onTap: () {
+                        //   debugPrint("Accion para editar el contacto");
+                        //   navigateToDetail(contact, 'Editar Contacto');
+                        // },
+                      ),
+                    );
+                  });
+            }),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () => {
+            Navigator.of(context)
+                .push(MaterialPageRoute(
+                    builder: (context) => FormContact(
+                          database: database,
+                        )))
+                .then((value) => reloadContacts())
+          },
+          label: const Text('Añadir'),
+          icon: const Icon(Icons.person_add),
+        ));
   }
 
   void navigateToDetail(Contact contact, String title) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => FormContact(database: database, appBarTitle: title, contactEdit: contact),)).then((value) => {
-      reloadContacts(),
-      setState(() {})
-    });
+    Navigator.of(context)
+        .push(MaterialPageRoute(
+          builder: (context) => FormContact(
+              database: database, appBarTitle: title, contactEdit: contact),
+        ))
+        .then((value) => {reloadContacts(), setState(() {})});
   }
 
   double roundDouble(double value, int places) {
@@ -106,7 +117,8 @@ class ContactHomeState extends State<ContactHomePages> {
     if (contacts.isEmpty) return 0.0;
     double sum = 0;
     for (var contact in contacts) {
-      sum += int.parse((contact.edad ?? "").isNotEmpty ? contact.edad.toString() : "0");
+      sum += int.parse(
+          (contact.edad ?? "").isNotEmpty ? contact.edad.toString() : "0");
     }
     return sum / contacts.length;
   }
