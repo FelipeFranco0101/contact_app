0b25bc7143bed4a21c60829ff26b518e3e887c03
Danielavg07 <132962388+Danielavg07@users.noreply.github.com>
2023-06-04 11:25:06 -0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,14 @@
-import 'package:contact_app/database/database_helper.dart';
-import 'package:contact_app/welcome_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
-
-void main() {
-  createDatabase();
-  runApp(const MyApp());
-}
-
-Database? database;
-late DatabaseHelper databaseHelper;
-
-void createDatabase() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  databaseHelper = DatabaseHelper.instance;
-  // Inicializar la base de datos
-  await DatabaseHelper.instance.initializeDB().then((value) => database = value);
-  databaseHelper.initializeDB().then((value) => database = value);
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
+ @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
-    );
-  }
-}
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio de sesión'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                "assets/background_image.jpg"), // Ruta de la imagen de fondo
+            fit: BoxFit.cover,
+          ),
+        ),
